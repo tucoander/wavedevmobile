7743cadfb45c881c283073038acd63208d1931fd
master <tuco.anderson@gmail.com>
2020-01-16 22:08:31 -0300

    criando marcador

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,61 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 // import { Container } from './styles';
 
- function Main() {
-  return (
-    <View />
-  );
-}
+function Main() {
+    const [currentRegion, setCurrentRegion] = useState(null);
 
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05,
+                })
+            }
+
+        }
+        loadInitialPosition();
+    }, []);
+
+    if (!currentRegion) {
+        return null;
+    }
+
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude: -23.163165, longitude: -46.9636841 }} >
+                <Image style={styles.avatar} source={{uri: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4' }} />
+            </Marker>
+        </MapView>
+    );
+}
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff',
+        alignItems: 'center',
+    }
+});
 export default Main;
+
+// parei no 59:59 da aula
